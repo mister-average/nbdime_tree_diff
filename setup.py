9bd43f90842a2f74383f1c1d1c45269b0dd0c218
mister-average <35749636+mister-average@users.noreply.github.com>
2022-10-11 21:23:26 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nbtime_tree_diff',
+    version='0.0.1',
+    description='Extension for classic jupyter allowing nbdiff-web between two notebooks in the file tree',
+    url='http://github.com/mister-average/nbtime_tree_diff',
+    author='mister-average',
+    author_email='mister_person@averageaddress.com',
+    license='BSD',
+    packages=find_packages(),
+    python_requires='>=3.4'
+)
